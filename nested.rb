beae388a3fa2d74b2086f31bf5f8e17c5bf8b353
Richard Huelves <rhuelves@gmail.com>
2019-08-22 01:56:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,10 @@ def hopper
           :languages => ["C"]
         }
      }
-
+hopper = programmer_hash[:grace_hopper]
 
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+#programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,6 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
     
 end
